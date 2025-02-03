046961c4134f57e3c303517069944e7575147d6e
Sakal Samnang <samnang.sakal@gmail.com>
2025-02-03 20:53:34 +0700

    init

--- src/app/api/purchase-subscription/route.ts
+++ src/app/api/purchase-subscription/route.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import { getToken } from 'next-auth/jwt';
-import { NextRequest, NextResponse } from 'next/server';
+import axios from "axios";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
 const secret = process.env.NEXTAUTH_SECRET;
 
@@ -9,13 +9,20 @@ export async function POST(req: NextRequest) {
     const token = await getToken({ req, secret });
 
     if (!token?.accessToken) {
-      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
     // Parse the request body
     const body = await req.json();
-    console.log('Request body:', body);
-    const { card_type, four_digits, expiry_month, expiry_year, month_number, id } = body;
-
+    // console.log('Request body:', body);
+    const {
+      card_type,
+      four_digits,
+      expiry_month,
+      expiry_year,
+      month_number,
+      id,
+    } = body;
+    console.log("duma send purchase here");
     // Make the request to your backend API, excluding the tenant field
     const response = await axios.post(
       `${process.env.API_URL}/subscription/purchase`,
@@ -30,25 +37,28 @@ export async function POST(req: NextRequest) {
       }),
       {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${token.accessToken}`,
         },
       },
     );
 
-    console.log('Response:', response.data);
+    console.log("Response:", response.data);
 
     return NextResponse.json(response.data, { status: 200 });
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
-      console.error('Axios error:', error.response?.data);
+      console.error("Axios error:", error.response?.data);
       return NextResponse.json(
         { message: error.message, error: error.response?.data },
         { status: error.response?.status || 500 },
       );
     } else {
-      console.error('Server error:', error);
-      return NextResponse.json({ message: 'Internal Server Error', error: (error as Error).message }, { status: 500 });
+      console.error("Server error:", error);
+      return NextResponse.json(
+        { message: "Internal Server Error", error: (error as Error).message },
+        { status: 500 },
+      );
     }
   }
 }
